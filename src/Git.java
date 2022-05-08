4b117c86f2bbe173303ec597b1a636bf3c0ce944
kamaldivyaprabha <kamal458mail@gmail.com>
2022-05-08 11:00:32 +0530

    master

---
+++ src/Git.java
@@ -0,0 +1,35 @@
+
+public class Git {
+
+	
+		public static void main(String[] args) {
+			
+			
+			String s ="kamal";
+			
+			StringBuilder builder = new StringBuilder();
+			StringBuilder append = builder.append(s);
+			
+			StringBuilder reverse = append.reverse();
+			System.out.println(reverse);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+			 
+	
+
+}
